ad49595b65a45599683e7f57b98973d47a24bc96
George <george.stemmann@gmail.com>
2022-02-25 00:24:26 -0800

    cool blogly stuff

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
+
+
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'ihaveasecret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
 
-connect_db(app)
-app.config['SECRET KEY'] = 'secret'
-debug = DebugToolbarExtension(app)
+connect_db(app) 
 db.create_all()
 
 @app.route('/users', methods=['GET'])
 def show_all_users():
-    list = [1,2,3,4,4,5,6,7,7,7,7,7,774532,231,1,2]
-    blue = [x for x in list if x > 4]
     users = User.query.all()
-    return render_template('user_listing.html', blue = blue, users = users)
+    return render_template('user_listing.html', users = users)
 
-@app.route('/users/new', methods=['GET'])
-def show_login_form():
-    return render_template('create_account.html')
 
-@app.route('/users/new', methods=['POST'])
+@app.route('/users', methods=['POST'])
 def users_new():
     new_user = User(
         first_name=request.form['first_name'],
@@ -32,16 +31,68 @@ def users_new():
 
     db.session.add(new_user)
     db.session.commit()
-    return redirect('/users')
+    return redirect(f"users/{new_user.user_id}")
 
-@app.route('/users/<int:user_id>/show', methods=['GET'])
-def show_edit_user_page():
-    return render_template('show_user.html')
+
+
+@app.route('/users/<int:user_id>/', methods=['GET'])
+def show_user_page(user_id):
+
+    user = User.query.get_or_404(user_id)
+    return render_template('show_user.html', user = user)
+
 
 @app.route('/users/<int:user_id>/edit', methods=['POST'])
-def show_edit_user_page():
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('edit_user.html', user = user)
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def update_user_info(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+    db.session.add(user)
+    db.session.commit()
     return redirect('user_listing.html')
 
-@app.route('/users/<int:user_id>/delete', methods=['GET'])
-def delete_user():
-    return render_template ('user_listing.html')
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return render_template('delete.html')
+
+@app.route('/users/<int:user_id>/create/new', methods=['POST'])
+def posts_new_form(user_id):
+    """Show a form to create a new post for a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('create_post.html', user=user)
+
+@app.route('/users/<int:user_id>/create/new', methods=['POST'])
+def create_post(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['post_title'],
+        content=request.content['post_content'],
+        user = user)
+    db.session.add(new_post)
+    db.session.commit()
+    return (f"/users/{user_id}")
+
+# GET /users/[user-id]/posts/new
+# Show form to add a post for that user.
+# POST /users/[user-id]/posts/new
+# Handle add form; add post and redirect to the user detail page.
+# GET /posts/[post-id]
+# Show a post.
+
+# Show buttons to edit and delete the post.
+
+# GET /posts/[post-id]/edit
+# Show form to edit a post, and to cancel (back to user page).
+# POST /posts/[post-id]/edit
+# Handle editing of a post. Redirect back to the post view.
+# POST /posts/[post-id]/delete
+# Delete the post.
--- models.py
+++ models.py
@@ -4,11 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def connect_db(app):
-    """Connect to database."""
-
-    db.app = app
-    db.init_app(app)
+default_img = 'https://picsum.photos/200/300.jpg'
 
 
 class User(db.Model):
@@ -19,7 +15,7 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    id = db.Column(db.Integer,
+    user_id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
     first_name = db.Column(db.String(50),
@@ -28,6 +24,37 @@ class User(db.Model):
     last_name = db.Column(db.String(50),
                      nullable=False,
                      unique=True)
-    image_url = db.Column(db.String(100),
-                    nullable=False, unique=True)
+    image_url = db.Column(db.String(500),
+                    unique=True, default = default_img)
+    
+    # post = db.relationship('Post')
+def connect_db(app):
+    """Connect to database."""
 
+    db.app = app
+    db.init_app(app)
+
+
+class Post(db.Model):
+    """Post"""
+    # def __repr__(self):
+    #     p = self
+
+
+    __tablename__ = "posts"
+
+    post_id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.String(50),
+                     nullable=False,
+                     unique=True)
+    content = db.Column(db.String(50),
+                     nullable=False,
+                     unique=True)
+    created_at = db.Column(db.String(100),
+                    nullable=False, unique=True)
+    user_id = db.column(
+                db.Text, 
+                db.ForeignKey('users.user_id'))
+                    
